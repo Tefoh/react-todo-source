6fdba19af085da1f04b2600bb01b67fb11e31ce9
Tefoh <tefo.ha27@gmal.com>
2021-04-10 01:27:15 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([
+    { text: 'Hit the gym', check: false },
+    { text: 'Pay bills', check: true },
+    { text: 'Meet George', check: false },
+    { text: 'Buy eggs', check: true },
+    { text: 'Read a book', check: false },
+    { text: 'Organize office', check: false },
+  ])
+
+  function handleInputChange(event) {
+    setText(event.target.value)
+  }
+
+  function addTodo() {
+    const newTodo = { text: text, check: false };
+
+    setTodos([...todos, newTodo]);
+
+    setText('');
+  }
+
+  const handleCheckTodo = (index) => {
+    setTodos(
+      todos.map((todo, todoIndex) => {
+        if (todoIndex === index) {
+          todo.check = !todo.check;
+        }
+        return todo;
+      })
+    )
+  }
+
+  const handleRemoveTodo = (event, index) => {
+    event.stopPropagation();
+
+    setTodos(
+      todos.filter((todo, todoIndex) => todoIndex !== index)
+    )
+  }
+
+  const todoElements = todos
+      .map((todo, index) => (
+        <li
+          className={todo.check ? 'checked' : ''}
+          onClick={() => handleCheckTodo(index)}
+          key={index}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {todo.text}
+          <span className="close" onClick={(event) => handleRemoveTodo(event, index)}>×</span>
+        </li>
+        )
+      )
+
+  return (
+    <>
+      <div className="header">
+        <h2 style={{ margin: 5 + 'px' }}>My To Do List</h2>
+        <input type="text" onChange={handleInputChange} value={text} />
+        <span className="addBtn" onClick={addTodo}>Add</span>
+      </div>
+      <ul>
+        {todoElements}
+      </ul>
+    </>
   );
 }
 
